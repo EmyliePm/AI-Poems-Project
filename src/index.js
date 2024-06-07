3d958f207ccae420a49d1df6120f2703fe1f657c
Emylie <167892046+EmyliePm@users.noreply.github.com>
2024-06-07 21:48:34 +0100

    added axios and AI API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 function generatePoem(event) {
   event.preventDefault();
+  let apiKey = "94fea4075b9658cte3d8o7c065a17914";
+  let prompt = "Generate a hilarious poem";
+  let context = "Be funny";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}}`;
 
+  console.log("generating poem");
+
+  axios.get(apiURL).then(displayPoem);
+}
+
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "funny poem",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
