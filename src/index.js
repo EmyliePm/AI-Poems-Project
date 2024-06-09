9bc54c6d0b56e1fd921cb459b55cab59d6a1be06
Emylie <167892046+EmyliePm@users.noreply.github.com>
2024-06-09 22:33:57 +0100

    added more instructions to context of AI

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function generatePoem(event) {
   let apiKey = "94fea4075b9658cte3d8o7c065a17914";
   let prompt = `Generate a hilarious poem about ${instructionsInput.value}`;
   let context =
-    "You are a hilarious comedian, with a mission to generate a short poem. Follow the user instructions";
+    "You are a hilarious comedian, with a mission to generate a short poem. Follow the user instructions. Try and make it rhyme..";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("generating poem");
